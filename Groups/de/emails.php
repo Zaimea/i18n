13ae55d3b1cdf35a9546d066b0e8aed57950f39c
Custură Laurențiu <127675571+custura@users.noreply.github.com>
2025-07-05 04:33:32 +0200

    Update emails.php

--- Groups/de/emails.php
+++ Groups/de/emails.php
@@ -7,12 +7,12 @@
 */
 
 return [
-    'accept_invitation' => 'Accept Invitation',
-    'accept_invitation_by_clicking' => 'You may accept this invitation by clicking the button below:',
-    'already_have_an_account' => 'If you already have an account, you may accept this invitation by clicking the button below:',
-    'create_account' => 'Create Account',
-    'no_account' => 'If you do not have an account, you may create one by clicking the button below. After creating an account, you may click the invitation acceptance button in this email to accept the group invitation:',
-    'not_expect_to_recive_an_invitation' => 'If you did not expect to receive an invitation to this group, you may discard this email.',
-    'you_have_been_invited_to_join' => 'You have been invited to join the :group group!',
-    'you_have_been_invited_to_join_as_client' => 'You have been invited to join the :group group as client!',
+    'accept_invitation' => 'Einladung annehmen',
+    'accept_invitation_by_clicking' => 'Sie können diese Einladung annehmen, indem Sie auf die Schaltfläche unter klicken:',
+    'already_have_an_account' => 'Wenn Sie bereits ein Konto haben, können Sie diese Einladung annehmen, indem Sie auf die Schaltfläche unten klicken:',
+    'create_account' => 'Konto erstellen',
+    'no_account' => 'Wenn Sie noch kein Konto haben, können sie eines erstellen, indem Sie auf die Schaltfläche unten klicken. Nachdem Sie ein Konto erstellt haben, können Sie in dieser E-Mail auf die Schaltfläche "Einladung annehmen" klicken, um die Gruppeneinladung anzunehmen:',
+    'not_expect_to_recive_an_invitation' => 'Wenn Sie keine Einladung zu dieser Gruppe erwartet haben, können Sie diese E-Mail verwerfen.',
+    'you_have_been_invited_to_join' => 'Sie wurden zur Gruppe :group eingeladen!',
+    'you_have_been_invited_to_join_as_client' => 'Sie wurden als Kunde zur Gruppe :group eingeladen!',
 ];
